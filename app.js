ff33e54ed556b42b83057502422ef9614c4d2212
unknown <svyat.teterin@gmail.com>
2023-06-15 17:05:04 +0300

    разработка второго приложения

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const App = {
         return {
             appPlaceholder: 'Введите название заметки',
             title: 'Заметки',
-            appInputValue: ''
+            appInputValue: '',
+            appNotes: ['Заметка 1', 'Заметка 2']
         }
     },
     methods: {
         appInputChanger(e) {
             this.appInputValue = e.target.value
+        },
+        addNewAppNote() {
+            if (this.appInputValue !== '') {
+                this.appNotes.push(this.appInputValue)
+                this.appInputValue = ''
+            }
+        },
+        removeAppNote(index) {
+            this.appNotes.splice(index, 1)
         }
     },
 }
